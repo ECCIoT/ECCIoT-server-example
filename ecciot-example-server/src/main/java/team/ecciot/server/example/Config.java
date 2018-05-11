e6dc871f8d350f4820f1ea35a36f1e4e7596a726
Guijie Lee <landriesnidis@yeah.net>
2018-05-11 11:39:32 +0800

    全局配置信息

---
+++ ecciot-example-server/src/main/java/team/ecciot/server/example/Config.java
@@ -0,0 +1,10 @@
+package team.ecciot.server.example;
+
+public class Config {
+	public final static String RTC_ADDRESS = "127.0.0.1";
+	public final static int RTC_PORT = 19941;
+	
+	public final static String API_KEY = "123456789abcdef";
+	public final static String SECRET_KEY = "s123456";
+	
+}
