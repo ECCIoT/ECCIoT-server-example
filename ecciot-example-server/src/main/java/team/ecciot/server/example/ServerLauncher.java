a91c3ad315bb3f509652878716bffe3cf288ab37
Guijie Lee <landriesnidis@yeah.net>
2018-05-14 00:26:02 +0800

    添加提示信息

--- ecciot-example-server/src/main/java/team/ecciot/server/example/ServerLauncher.java
+++ ecciot-example-server/src/main/java/team/ecciot/server/example/ServerLauncher.java
@@ -1,6 +1,8 @@
 package team.ecciot.server.example;
 
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import team.ecciot.server.example.comm.CommClient;
 
@@ -10,14 +12,18 @@ import team.ecciot.server.example.comm.CommClient;
  */
 public class ServerLauncher 
 {
+	public static Logger LOGGER = LogManager.getLogger(ServerLauncher.class);
+	
     public static void main( String[] args ) throws Exception
     {
     	//自动快速地使用缺省Log4j环境
         BasicConfigurator.configure();
         
+        LOGGER.info("正在连接服务器……");
+        
         CommClient cc = new CommClient();
         cc.connect(Config.RTC_PORT, Config.RTC_ADDRESS);
         
-        System.out.println( "Hello World!" );
+        LOGGER.info("Start.");
     }
 }
