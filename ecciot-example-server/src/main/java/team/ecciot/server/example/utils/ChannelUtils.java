3919318b5b07113d04c46b188f99177cc908829d
Guijie Lee <landriesnidis@yeah.net>
2018-05-15 21:29:02 +0800

    消息发送时打印日志

--- ecciot-example-server/src/main/java/team/ecciot/server/example/utils/ChannelUtils.java
+++ ecciot-example-server/src/main/java/team/ecciot/server/example/utils/ChannelUtils.java
@@ -1,9 +1,16 @@
 package team.ecciot.server.example.utils;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import io.netty.channel.Channel;
 
 public class ChannelUtils {
+	
+	public static Logger LOGGER = LogManager.getLogger(ChannelUtils.class);
+	
 	public static void sendMessage(Channel channel,String msg){
+		LOGGER.info("[发送数据] → msg:" + msg);
 		channel.write(msg);
 		channel.writeAndFlush("\n");
 	}
